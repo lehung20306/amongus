54231d299fbf978ce23cdb7f4961618d9aa3074a
hungle <lehung20306@gmail.com>
2026-03-23 23:18:50 +0700

    test

---
+++ Main.java
@@ -0,0 +1,8 @@
+import goi1.Hung;
+
+public class Main {
+    public static void main(String[] args){
+        Hung h = new Hung(20);
+        h.xinchao();
+    }
+}
---
+++ goi1/Hung.java
@@ -0,0 +1,11 @@
+package goi1;
+
+public class Hung {
+    int tuoi;
+    public Hung(int tuoi){
+        this.tuoi = tuoi;
+    }
+    public void xinchao(){
+        System.out.println(this.tuoi);
+    }
+}
